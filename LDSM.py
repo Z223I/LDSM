e7a26cdd6db66b0be54a6d38f36352fea8df8b20
Z223I <BruceWilsonPH@yahoo.com.ph>
2018-01-16 19:03:34 -0600

    Put class back in.

--- LDSM.py
+++ LDSM.py
@@ -1,18 +1,21 @@
 from transitions import Machine
 
+class LDSM():
+    def test(self):
+        states = ['A', 'B', 'C', 'D']
+        # Define with list of lists
+        transitions = [
+            ['walk', 'A', 'B'],
+            ['run', 'B', 'C'],
+            ['sprint', 'C', 'D']
+        ]
 
-states = ['A', 'B', 'C', 'D']
-# Define with list of lists
-transitions = [
-    ['walk', 'A', 'B'],
-    ['run', 'B', 'C'],
-    ['sprint', 'C', 'D']
-]
-m = Machine(states=states, transitions=transitions, initial='A')
-m.to_C()
-m.sprint()
-if (m.state == 'D'):
-    print ( "Good" )
+        m = Machine(states=states, transitions=transitions, initial='A')
+        m.to_C()
+        m.sprint()
+        if (m.state == 'D'):
+            print ( "Good" )
 
 
-
+ldsm = LDSM()
+ldsm.test()
