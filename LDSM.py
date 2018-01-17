b95fec9cec4ad89ef92526e0a1edbd0521787692
Z223I <BruceWilsonPH@yahoo.com.ph>
2018-01-16 18:59:19 -0600

    Deleted the class.

--- LDSM.py
+++ LDSM.py
@@ -1,36 +1,18 @@
 from transitions import Machine
 
 
-class LDSM():
-    def test(self):
-        states = ['A', 'B', 'C', 'D']
-        # Define with list of dictionaries
-        transitions = [
-            {'trigger': 'walk', 'source': 'A', 'dest': 'B'},
-            {'trigger': 'run', 'source': 'B', 'dest': 'C'},
-            {'trigger': 'sprint', 'source': 'C', 'dest': 'D'}
-        ]
-        m = Machine(states=states, transitions=transitions, initial='A')
-        m.walk()
-        if (m.state == 'B'):
-            print ( "1. Good" )
-
-        # Define with list of lists
-        transitions = [
-            ['walk', 'A', 'B'],
-            ['run', 'B', 'C'],
-            ['sprint', 'C', 'D']
-        ]
-        m = Machine(states=states, transitions=transitions, initial='A')
-        m.to_C()
-        m.sprint()
-        if (m.state == 'D'):
-            print ( "2. Good" )
+states = ['A', 'B', 'C', 'D']
+# Define with list of lists
+transitions = [
+    ['walk', 'A', 'B'],
+    ['run', 'B', 'C'],
+    ['sprint', 'C', 'D']
+]
+m = Machine(states=states, transitions=transitions, initial='A')
+m.to_C()
+m.sprint()
+if (m.state == 'D'):
+    print ( "Good" )
 
 
 
-
-if __name__ == "__main__":
-
-    m = LDSM()
-    m.test()
